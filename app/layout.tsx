69e6f54e864c138ce24f556e75d29cbd5db1cb5d
mearko <76460780+markostrba@users.noreply.github.com>
2025-10-21 17:28:32 +0200

    style: update fonts and apply neobrutalism theme to Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
 import type { Metadata } from "next";
-import { Bricolage_Grotesque } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ClerkProvider } from "@clerk/nextjs";
 import ROUTES from "@/constants/routes";
+import { neobrutalism } from "@clerk/themes";
 
-const bricolage = Bricolage_Grotesque({
-  variable: "--font-bricolage",
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Converso",
+  title: "Sonexa - AI Teaching Companion",
   description: "Real-time AI Teaching Platform",
 };
 
@@ -22,9 +24,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>
+      <body
+        className={`
+          ${poppins.variable}
+          antialiased
+        `}
+      >
         <ClerkProvider
-          appearance={{ variables: { colorPrimary: "#fe5933" } }}
+          appearance={{
+            variables: { colorPrimary: "#ec489d" },
+            baseTheme: neobrutalism,
+          }}
           afterSignOutUrl={ROUTES.HOME}
         >
           <Navbar />
